43959945bb868e6f498adf7c07eb0bbc792868ed
jaemin.han <jaemin.han@nyu.edu>
2018-05-14 17:18:36 -0500

    header toggle initial

--- app/index.js
+++ app/index.js
@@ -1,52 +1,45 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-// require('./index.css');
-var PropTypes = require('prop-types');
+require('./index.css');
+// var PropTypes = require('prop-types');
+var App = require('./components/App');
 
 // creating first react
 
 // State
 // Lifecycle Event
 // UI
-// class App extends React.Component {
+
+
+// class Badge extends React.Component {
 //     render() {
 //         return (
 //             <div>
-//                 Hello React World!
+//                 <img 
+//                     src={this.props.img}
+//                     alt='Avatar'
+//                     style={{width: 100, height: 100}}
+//                 />
+//                 <h1>Name: {this.props.name}</h1>
+//                 <h3>username: {this.props.username}</h3>
 //             </div>
 //         )
 //     }
 // }
 
-class Badge extends React.Component {
-    render() {
-        return (
-            <div>
-                <img 
-                    src={this.props.img}
-                    alt='Avatar'
-                    style={{width: 100, height: 100}}
-                />
-                <h1>Name: {this.props.name}</h1>
-                <h3>username: {this.props.username}</h3>
-            </div>
-        )
-    }
-}
-
-Badge.propTypes = {
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired
-}
+// Badge.propTypes = {
+//     img: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     username: PropTypes.string.isRequired
+// }
 
 
 ReactDOM.render(
-    // <App />,
-    <Badge
-        name='Jaemin'
-        username='jaemin-han'
-        img={'https://avatars3.githubusercontent.com/u/19918594?s=460&v=4'}
-    />,
+    <App />,
+    // <Badge
+    //     name='Jaemin'
+    //     username='jaemin-han'
+    //     img={'https://avatars3.githubusercontent.com/u/19918594?s=460&v=4'}
+    // />,
     document.getElementById('app')
 );
