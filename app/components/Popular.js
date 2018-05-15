9f9576420f459fb0ae28a0e4a199e6dc08d58f3b
jaemin.han <jaemin.han@nyu.edu>
2018-05-15 11:09:03 -0500

    selected language onClick implemented

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,36 @@
 var React = require('react');
 
 class Popular extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(lang) {
+        this.setState(function() {
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
     render() {
 
         var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
         return (
             <ul className="languages">
+                <p>Selected Language: {this.state.selectedLanguage}</p>
                 {languages.map(function (lang) {
                     return (
-                        <li>
+                        <li 
+                            onClick={this.updateLanguage.bind(null, lang)}
+                            key={lang}>
                             {lang}
                         </li>
                     )
-                })}
+                }, this)}
             </ul>
         );
     }
