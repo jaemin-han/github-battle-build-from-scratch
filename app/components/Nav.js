8f9e03c4e4cc5404bc260b7e319da62c667ae264
jaemin.han <jaemin.han@nyu.edu>
2018-05-18 18:19:40 -0400

    react router in progress

---
+++ app/components/Nav.js
@@ -0,0 +1,28 @@
+var React = require('react');
+// var Link = require('react-router-dom').Link
+var NavLink = require('react-router-dom').NavLink;
+
+funtion Nav() {
+    return (
+        <ul className='nav'>
+            <li>
+                <NavLink activeClassName='active' to='/'>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName='active' to='/battle'>
+                    Battle
+                </NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName='active' to='/popular'>
+                    Popular
+                </NavLink>
+            </li>
+
+        </ul>
+    )
+}
+
+module.exports = Nav;
