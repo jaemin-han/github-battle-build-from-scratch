2fe88fed7109da4d864000fd42e2cbb091cd9313
jaemin.han <jaemin.han@nyu.edu>
2018-05-21 16:42:59 -0400

    dynamically render style based on username

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,33 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+//stateless functional component
+function PlayerPreview(props) {
+    return (
+        <div>
+            <div className="column">
+                <img
+                    className="avatar"
+                    src={props.avatar}
+                    alt={'Avatar for ' + props.username}
+                />
+                <h2 className="username">@{props.username}</h2>
+            </div>
+            <button
+                className="reset"
+                onClick={props.onReset.bind(null, props.id)}>
+                    Reset
+            </button>
+        </div>
+    )
+}
+
+PlayerPreview.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired
+}
 
 class PlayerInput extends React.Component {
     constructor() {
@@ -92,6 +119,8 @@ class Battle extends React.Component {
     render() {
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
+        var playerOneImage = this.state.playerOneImage;
+        var playerTwoImage = this.state.playerTwoImage;
 
         return (
             <div>
@@ -102,14 +131,35 @@ class Battle extends React.Component {
                             id="playerOne"
                             label="Player One" 
                             onSubmit={this.handleSubmit}
-                        />}
+                        />
+                    }
+
+                    {playerOneImage !== null &&
+                        <PlayerPreview
+                            avatar={playerOneImage}
+                            username={playerOneImage}
+                            onReset={this.handleReset}
+                            id="playerOne"
+                        />
+                    }
 
                     {!playerTwoName &&
                         <PlayerInput
                             id="playerTwo"
                             label="Player Two" 
                             onSubmit={this.handleSubmit}
-                        />}
+                        />
+                    }
+
+                    {playerTwoImage !== null &&
+                        <PlayerPreview
+                            avatar={playerTwoImage}
+                            username={playerTwoImage}
+                            onReset={this.handleReset}
+                            id="playerTwo"
+                        />
+                    }
+
                 </div>
             </div>
         )
