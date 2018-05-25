d58ec6135cfb0e6c140bb18c968939cc73fd96a9
jaemin.han <jaemin.han@nyu.edu>
2018-05-25 15:45:24 -0400

    ready for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
+var webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+// NODE_ENV to production
+// Uglify code
+
+
+// "module.exports" local testing
+// module.exports = {
+  var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,14 +22,43 @@ module.exports = {
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  },
   //instead of requesting server from /popular, it will redirect to this and react router will redirect to /popular
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-  ],
-  mode: "development"
+  ]
+  // mode: "development"
 };
+
+// push to production and uglify code - additional step to take to happen
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimization.minimize()
+  )
+}
+
+module.export = config;
