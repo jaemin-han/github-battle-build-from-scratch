799ddd5645f1c2484fe036fe8a190d4fe3662348
jaemin.han <jaemin.han@nyu.edu>
2018-06-05 15:06:50 -0400

    webpack and api.js es6 major update config

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,28 +1,29 @@
-var axios = require('axios');
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec;
+
+const axios = require('axios');
+
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+// Sting Template
+const params = `?client_id=${id}&client_secret=${sec}`;
 
 // PROMISE
 // Make a 'Get' request to the url below
 // Axios will return a promise by returning an object that has a .then() property
 function getProfile (username) {
-  return axios.get('https://api.github.com/users/' + username + params)
+  return axios.get(`https://api.github.com/users/${username}${params}`)
     // When this function is invoked, it's going to pass a user or whatever we get from github api
     // but it will only going to be invoked after we get the information back
-    .then(function (user) {
-      return user.data;
-    });
+    .then(({ data }) => data);
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
 }
-// basically when you get the information back from github api, then... call the function and pass us whatever the information (in this case User)
+
 function getStarCount (repos) {
-  return repos.data.reduce(function (count, repo) {
-    return count + repo.stargazers_count
-  }, 0);
+  // basically when you get the information back from github api, then... call the function and pass us whatever the information (in this case User)
+  return repos.data.reduce((count, { stargazers_count }) => count +stargazers_count, 0);
+  // Arrow function and implicit return
 }
 // Reduce - allows us to take an array and reduce it to a single value
 // Initially pass a ZERO
@@ -32,12 +33,8 @@ function getStarCount (repos) {
 // once there is no more item to iterate over, we're going to have a total number or how many stars
 // are in repos.data
 
-
-function calculateScore (profile, repos) {
-  var followers = profile.followers;
-  var totalStars = getStarCount(repos);
-
-  return (followers * 3) + totalStars;
+function calculateScore ({ followers }, repos) {
+  return (followers * 3) + getStarCount(repos);
 }
 
 function handleError (error) {
@@ -46,42 +43,38 @@ function handleError (error) {
 }
 
 function getUserData (player) {
-  return axios.all([
+  // Native Promises for ES6
+  return Promise.all([
     getProfile(player),
     getRepos(player)
-  ]).then(function (data) {
-    var profile = data[0];
-    var repos = data[1];
-
-    return {
-      profile: profile,
+    // Arrow function, destructuring arrays and returning object
+  ]).then(([profile, repos ]) => ({
+    //Shorthand notation
+      profile,
       score: calculateScore(profile, repos)
-    }
-  });
+  }))
 }
-
+// Arrow function and implicit return
 function sortPlayers (players) {
-  return players.sort(function (a,b) {
-    return b.score - a.score;
-  });
+  return players.sort((a,b) => b.score - a.score);
 }
 
 module.exports = {
-  battle: function (players) {
-    return axios.all(players.map(getUserData))
+  battle (players) {
+    return Promise.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError);
   },
-    // battle to return a promise. When it is resolved, it will going to have all players' info
+      // battle to return a promise. When it is resolved, it will going to have all players' info
     // map over players, 'getUserData' which will call 'getProfile' and 'getRepos'.
     // once we get those info, return 'profile' and 'score' objects
     // then sort array in the 'sortPlayers' function
-  fetchPopularRepos: function (language) {
-    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
-    return axios.get(encodedURI)
-      .then(function (response) {
-        return response.data.items;
-      });
+  // Removed function keywords
+  fetchPopularRepos (language) {
+    // Using template string
+    const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+    return axios.get(encodedURI).then(({ data }) => data.items);
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
+
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-// NODE_ENV to production
-// Uglify code
-
-
-// "module.exports" local testing
-// module.exports = {
-  var config = {
-  entry: './app/index.js',
+var config = {
+  entry: ['babel-polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
@@ -22,22 +16,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
-  optimization: {
-    minimizer: [
-      // we specify a custom UglifyJsPlugin here to get source maps in production
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true
-        },
-        sourceMap: true
-      })
-    ]
-  },
-  //instead of requesting server from /popular, it will redirect to this and react router will redirect to /popular
   devServer: {
     historyApiFallback: true,
   },
@@ -46,10 +24,8 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
       template: 'app/index.html'
     })
   ]
-  // mode: "development"
 };
 
-// push to production and uglify code - additional step to take to happen
 if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
     new webpack.DefinePlugin({
@@ -57,8 +33,8 @@ if (process.env.NODE_ENV === 'production') {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       }
     }),
-    new webpack.optimization.minimize()
+    new webpack.optimize.UglifyJsPlugin()
   )
 }
 
-module.export = config;
+module.exports = config;
